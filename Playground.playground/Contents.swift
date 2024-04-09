87e5e403169b2b4ec572daa7e7842e24dd4997ca
Luann Luna <luann.luna@thoughtworks.com>
2024-04-09 15:02:09 -0300

    threesum

--- Playground.playground/Contents.swift
+++ Playground.playground/Contents.swift
@@ -1,22 +1,60 @@
 import Foundation
 
 class Solution {
-    func maxArea(_ height: [Int]) -> Int {
-        var lhs = 0
-        var rhs = height.count-1
-        var maxValue = 0
-        
-        while lhs < rhs {
-            let area = min(height[lhs], height[rhs]) * (rhs - lhs)
-            maxValue = max(maxValue, area)
-            if height[lhs] < height[rhs] {
-                lhs += 1
-            } else {
-                rhs -= 1
+    func threeSum(_ numbers: [Int]) -> [[Int]] {
+        var values = [Int: [(Int, Int)]]()
+        for (index, number) in numbers.enumerated() {
+            var temp: [Int] = numbers
+            temp.remove(at: index)
+            let target = 0 - number
+            let two = twoSum(temp, target)
+            values[number, default: []].append(contentsOf: two)
+        }
+        var result: Set<[Int]> = Set()
+        values.forEach { elm in
+            elm.value.forEach { (A, B) in
+                result.insert([elm.key, A, B])
             }
         }
-        return maxValue
+        
+        let array = Array(result)
+        return removeDuplicateRows(from: array)
+    }
+    
+    func twoSum(_ numbers: [Int], _ target: Int) -> [(Int,Int)] {
+        var indices = [Int: Int]()
+        var result = [(Int, Int)]()
+        
+        for (index, number) in numbers.enumerated() {
+            let complement = target - number
+            
+            if let complementIndex = indices[complement] {
+                result.append((numbers[complementIndex], number))
+            }
+            indices[number] = index
+        }
+        
+        return result
+    }
+    
+    func removeDuplicateRows(from matrix: [[Int]]) -> [[Int]] {
+        var rowSet = Set<[Int]>()
+        matrix.forEach {
+            rowSet.insert($0.sorted())
+        }
+        return Array(rowSet)
     }
 }
-//                        0 1 2 3 4 5 6 7 8
-print(Solution().maxArea([1,8,6,2,5,4,8,3,7]) == 49)
+
+//let A = Solution().threeSum([-1,0,1,2,-1,-4])
+//print(A)
+//print(A == [[-1,-1,2],[-1,0,1]])
+
+var A = Solution().threeSum([-1,0,1,2,-1,-4,-2,-3,3,0,4])
+
+for index in 0..<A.count {
+    A[index] = A[index].sorted()
+}
+print(A.count)
+print([[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]].count)
+print(A == [[-4,0,4],[-4,1,3],[-3,-1,4],[-3,0,3],[-3,1,2],[-2,-1,3],[-2,0,2],[-1,-1,2],[-1,0,1]])
